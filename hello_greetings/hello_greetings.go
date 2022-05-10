b6d73bb53dfacaeed9896fa18e678ba930e2d279
U01A423F <u01a423f@lacaixa.es>
2022-05-10 14:40:36 +0200

    greetings advanced (same tutorial)

--- hello_greetings/hello_greetings.go
+++ hello_greetings/hello_greetings.go
@@ -1,5 +1,7 @@
 package main
 
+// ----------------------------------------------------------------------------------
+
 // import (
 // 	"fmt"
 
@@ -11,6 +13,66 @@ package main
 // 	message := greetings.Hello("Gladys")
 // 	fmt.Println(message)
 // }
+
+// ----------------------------------------------------------------------------------
+
+// import (
+// 	"fmt"
+// 	"log"
+
+// 	"example.com/greetings"
+// )
+
+// func main() {
+// 	// Set properties of the predefined Logger, including
+// 	// the log entry prefix and a flag to disable printing
+// 	// the time, source file, and line number.
+// 	log.SetPrefix("greetings: ")
+// 	log.SetFlags(0)
+
+// 	// Request a greeting message.
+// 	message, err := greetings.Hello("")
+// 	// If an error was returned, print it to the console and
+// 	// exit the program.
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	// If no error was returned, print the returned message
+// 	// to the console.
+// 	fmt.Println(message)
+// }
+
+// ----------------------------------------------------------------------------------
+
+// import (
+// 	"fmt"
+// 	"log"
+
+// 	"example.com/greetings"
+// )
+
+// func main() {
+// 	// Set properties of the predefined Logger, including
+// 	// the log entry prefix and a flag to disable printing
+// 	// the time, source file, and line number.
+// 	log.SetPrefix("greetings: ")
+// 	log.SetFlags(0)
+
+// 	// Request a greeting message.
+// 	message, err := greetings.Hello("Gladys")
+// 	// If an error was returned, print it to the console and
+// 	// exit the program.
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	// If no error was returned, print the returned message
+// 	// to the console.
+// 	fmt.Println(message)
+// }
+
+// ----------------------------------------------------------------------------------
 
 import (
 	"fmt"
@@ -26,15 +88,15 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("")
-	// If an error was returned, print it to the console and
-	// exit the program.
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// If no error was returned, print the returned message
-	// to the console.
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
